e57873e7769912d5d6cea3ba64dc47b61afaff77
Mohiit007 <164232278+Mohiit007@users.noreply.github.com>
2025-08-04 22:06:45 +0530

    Delete predict.py

--- predict.py
+++
@@ -1,7 +0,0 @@
-from ultralytics import YOLO
-
-# Load your trained model
-model = YOLO('best.pt')
-
-# Run predictions on test images
-results = model.predict(source='path_to_test_images', save=True)
